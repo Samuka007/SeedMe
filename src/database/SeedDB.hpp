eaed0f0502da1c033c9d2cf138aede92f9ea747c
Samuka007 <samuka007@qq.com>
2023-05-20 19:49:00 +0800

    Summerized

--- src/database/SeedDB.hpp
+++ src/database/SeedDB.hpp
@@ -1,11 +1,14 @@
 #ifndef SeedDB_hpp
 #define SeedDB_hpp
 
+#include <vector>
+
 #include "./dataframe/DataPre.hpp"
 #include "./userframe/UserPre.hpp"
 #include "./frame/Metadata.hpp"
+#include "./Search.hpp"
 
-class SeedDB : DataPre, UserPre, Metadata {
+class SeedDB : public DataPre, public UserPre, public Metadata {
     public:
         SeedDB(std::string filename)
         :   DataPre(filename+".data"s), 
@@ -13,33 +16,75 @@ class SeedDB : DataPre, UserPre, Metadata {
             Metadata(filename+".meta"s)
         {}
 
-        namespace UserControl{
-            void CreateUser();
-
-            void UpdateUser();
-
-            void GetUserSource();
-
-            void DeleteUser();
+        std::vector<uint32_t> src_name(std::string_view name){
+            //使用正则表达式与分词
+            bool a = true;
         }
 
-        namespace SrcControl{
-            void CreateSource();
-
-            void GetSource();
-
-            void UpdateSource();
-
-            void DeleteSource();
-        }        
+        bool magnet_exist(std::string_view magnet){
+            bool find = false;
+            for(uint32_t i=1;i<this->getSum();++i){
+                if(this->Deleted::contains(i))
+                    continue;
+                if(DataPre::getMagnet(this->row_data(i)) == magnet){
+                    find = true;
+                    break;
+                }
+            }
+            return find;
+        }
+        bool username_exist(std::string_view username){
+            bool find = false;
+            for(uint32_t i=1;i<this->getSum();++i){
+                if(this->Deleted::contains(i))
+                    continue;
+                if(DataPre::getUsername(this->row_data(i)) == username){
+                    find = true;
+                    break;
+                }
+            }
+            return find;
+        }
 
         /**
-        * store
-        * read
-        * generate 
-        * change metadata
+         * UserController
+        */
+        void CreateUser(
+            std::string_view username,
+            std::string_view password,
+            std::string_view password_varified){
+            if(username_exist(username)){
+                //throw error
+                return;
+            }
+            if(password != password_varified){
+                //throw error
+                return;
+            }
+            UserPre::addUser(username, password);
+        }
+
+        void UpdateUser();
+        void GetUserSource();
+        void DeleteUser();
+        
+        /**
+         * SourceController:
+        */
+        void CreateSource();
+        void GetSource();
+        void UpdateSource();
+        void DeleteSource();
+
+
+
+        /**--finished
+        * store-
+        * read-
+        * generate -
+        * change metadata-
         * 
-        * log metadata and store source
+        * log metadata and store source-
         * 
         */
 
@@ -51,6 +96,6 @@ class SeedDB : DataPre, UserPre, Metadata {
         *           - 100+ = user           all     1
         *       - tags(metadatas)
         */
-}
+};
 
 #endif
