3d41dee952fea2de78ebc2265c9133ea8178077a
Samuka007 <samuka007@qq.com>
2023-06-13 16:22:21 +0800

    delete build

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/samuka/workspace/SeedMe
-# Build directory: /home/samuka/workspace/SeedMe/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
